32cc85ebf9a34859a940abc4c1d335acb213fe81
George Crabtree <supercrabtree@googlemail.com>
2018-09-07 23:31:46 +1000

    Print files and directorys as blue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main () {
+    if let Ok(entries) = fs::read_dir(".") {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(metadata) = entry.metadata() {
+                    if metadata.is_dir() {
+                        println!("\x1B[34m{}\x1B[0m", entry.path().display());
+                    } else {
+                        println!("{}", entry.path().display());
+                    }
+                }
+            }
+        }
+    }
 }
